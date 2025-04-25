61982375acb4b023833e14bc43c49063fe2a2972
priya <tadepallipriyanjana@gmail.com>
2025-04-25 18:10:17 +0530

    Add quiz project files

---
+++ quiz/script.js
@@ -0,0 +1,19 @@
+function cpp(){
+  window.location.href="cpp.html"
+}
+
+function java(){
+  window.location.href="java.html"
+}
+
+function py(){
+  window.location.href="py.html"
+}
+
+function js(){
+  window.location.href="js.html"
+}
+
+function react(){
+  window.location.href="react.html"
+}
